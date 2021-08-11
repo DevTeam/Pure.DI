1f884dd546e7505746b81b4247cdefa3b1d5e552
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-08-11 22:25:48 +0300

    Add tests

---
+++ Pure.DI.UsageScenarios.Tests/AutowiringViaType.cs
@@ -0,0 +1,29 @@
+﻿namespace Pure.DI.UsageScenarios.Tests
+{
+    using Shouldly;
+    using Xunit;
+
+    public class AutowiringViaType
+    {
+        [Fact]
+        public void Run()
+        {
+            // $visible=false
+            // $tag=1 Basics
+            // $priority=01
+            // $description=Autowiring
+            // $header=Auto-wring is the most natural way to use containers. In the first step, we should create a container. At the second step, we bind interfaces to their implementations. After that, the container is ready to resolve dependencies.
+            // {
+            // Create the container and configure it, using full autowiring
+            DI.Setup()
+                .Bind<IDependency>().To<Dependency>()
+                .Bind<IService>().To<Service>();
+
+            // Resolve an instance of interface `IService`
+            var instance = AutowiringViaTypeDI.Resolve(typeof(IService));
+            // }
+            // Check the instance
+            instance.ShouldBeOfType<Service>();
+        }
+    }
+}
---
+++ Pure.DI.UsageScenarios.Tests/TagsWithType.cs
@@ -0,0 +1,38 @@
+﻿namespace Pure.DI.UsageScenarios.Tests
+{
+    using Shouldly;
+    using System.Diagnostics.CodeAnalysis;
+    using Xunit;
+
+    [SuppressMessage("ReSharper", "UnusedVariable")]
+    public class TagsWithType
+    {
+        [Fact]
+        public void Run()
+        {
+            // $visible=false
+            // $tag=1 Basics
+            // $priority=01
+            // $description=Tags
+            // $header=Tags are useful while binding to several implementations of the same abstract types.
+            // {
+            DI.Setup()
+                .Bind<IDependency>().To<Dependency>()
+                // Bind using several tags
+                .Bind<IService>().Tag(10).Tag("abc").To<Service>()
+                .Bind<IService>().To<Service>();
+
+            // Resolve instances using tags
+            var instance1 = (IService)TagsWithTypeDI.Resolve(typeof(IService), "abc");
+            var instance2 = (IService)TagsWithTypeDI.Resolve(typeof(IService), 10);
+
+            // Resolve the instance using the empty tag
+            var instance3 = (IService)TagsWithTypeDI.Resolve(typeof(IService));
+            // }
+            // Check instances
+            instance1.ShouldBeOfType<Service>();
+            instance2.ShouldBeOfType<Service>();
+            instance3.ShouldBeOfType<Service>();
+        }
+    }
+}
