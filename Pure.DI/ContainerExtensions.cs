2c56e12c062f2ffd940f781d2e3ba2324cdcd5fc
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-12-03 18:19:30 +0300

    Update deps

--- Pure.DI/ContainerExtensions.cs
+++ Pure.DI/ContainerExtensions.cs
@@ -12,17 +12,5 @@ namespace Pure.DI
             .Create()
             .Bind<CompilationDiagnostic>().Bind<IDiagnostic>().As(IoC.Lifetime.ContainerSingleton).To<CompilationDiagnostic>()
             .Container;
-        
-        /*public static void Log(string message)
-        {
-            try
-            {
-                System.IO.File.AppendAllText(@"C:\Projects\_temp\a\Composer\aa.txt", $"{message}\n");
-            }
-            catch
-            {
-                // ignored
-            }
-        }*/
     }
 }
