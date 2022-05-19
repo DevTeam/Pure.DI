d304e83be76074f092c7adeef96dce53dc2fe091
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2022-05-19 11:54:53 +0300

    Makes the default service provider singleton

--- Pure.DI/Features/AspNet.cs
+++ Pure.DI/Features/AspNet.cs
@@ -12,8 +12,11 @@ namespace NS35EBD81B.Features
         private static void Setup()
         {
             NS35EBD81B.DI.Setup("AspNetFeature")
+                .Bind<System.IServiceProvider>(typeof(NS35EBD81B.DefaultServiceProvider))
+                    .As(Lifetime.Singleton)
+                    .To(ctx => new NS35EBD81B.DefaultServiceProvider(type => Resolve(type)))
                 .Bind<System.IServiceProvider>()
-                    .To(ctx => NS35EBD81B.ServiceProviderInstance.ServiceProvider != null ? NS35EBD81B.ServiceProviderInstance.ServiceProvider: new NS35EBD81B.DefaultServiceProvider(type => Resolve(type)))
+                    .To(ctx => NS35EBD81B.ServiceProviderInstance.ServiceProvider != null ? NS35EBD81B.ServiceProviderInstance.ServiceProvider: ctx.Resolve<System.IServiceProvider>(typeof(NS35EBD81B.DefaultServiceProvider)))
                 .Bind<Microsoft.Extensions.Logging.ILogger<TT>>()
                     .To(ctx => (Microsoft.Extensions.Logging.ILogger<TT>)ctx.Resolve<System.IServiceProvider>().GetService(typeof(Microsoft.Extensions.Logging.ILogger<TT>)));
         }
