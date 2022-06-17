0e657affaf55b69fa72daaf8bac83f8adfb49773
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2022-06-17 18:46:29 +0300

    Minor fix in comments

--- Pure.DI.Templates/Templates/Pure.DI.Template.CSharp/IOutput.cs
+++ Pure.DI.Templates/Templates/Pure.DI.Template.CSharp/IOutput.cs
@@ -6,7 +6,7 @@ namespace _PureDIProjectName_
     internal interface IOutput
     {
         /// <summary>Writes a string to the text stream, followed by a line terminator.</summary>
-        /// <param line="value">The string to write. If <paramref name="line" /> is <see langword="null" />, only the line terminator is written.</param>
+        /// <param name="line">The string to write. If <paramref name="line" /> is <see langword="null" />, only the line terminator is written.</param>
         void WriteLine(string? line);
     }
 }
