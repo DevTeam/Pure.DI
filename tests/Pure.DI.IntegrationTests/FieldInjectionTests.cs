4a4febc3d5cfe6e87cc0f3ba830a34b43aaec85d
Nikolay Pyanikov <n.pyanikov@tinkoff.ru>
2023-03-21 10:41:45 +0300

    Support field injection

---
+++ tests/Pure.DI.IntegrationTests/FieldInjectionTests.cs
@@ -0,0 +1,90 @@
+﻿namespace Pure.DI.IntegrationTests;
+
+using System.Collections.Immutable;
+
+[Collection(nameof(NonParallelTestsCollectionDefinition))]
+public class FieldInjectionTests
+{
+    [Fact]
+    public async Task ShouldSupportFieldInjection()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using Pure.DI;
+
+namespace Sample
+{
+    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    internal class CustomOrdinalAttribute : Attribute
+    {
+        public readonly int Ordinal;
+
+        public CustomOrdinalAttribute(int ordinal)
+        {
+            Ordinal = ordinal;
+        }
+    }
+
+    interface IDependency {}
+
+    class Dependency: IDependency
+    {        
+    }
+
+    interface IService
+    {
+        IDependency Dep { get; }        
+
+        IDependency OtherDep0 { get; }
+
+        IDependency OtherDep1 { get; }
+    }
+
+    class Service: IService 
+    {
+        private IDependency _dep1;
+        [Ordinal(1)] public IDependency _otherDep1;           
+        [CustomOrdinal(0)] public IDependency _otherDep0;
+
+        public Service(IDependency dep)
+        { 
+            _dep1 = dep;           
+        }
+
+        public IDependency Dep => _dep1;
+        public IDependency OtherDep0 => _otherDep0;
+        public IDependency OtherDep1 => _otherDep1;                       
+    }
+
+    static class Setup
+    {
+        private static void SetupComposer()
+        {
+            DI.Setup("Composer")
+                .Bind<IDependency>().To<Dependency>()
+                .Bind<IService>().To<Service>()
+                .Root<IService>("Service")
+                .OrdinalAttribute<CustomOrdinalAttribute>();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composer = new Composer();
+            var service = composer.Service;
+            Console.WriteLine(service.Dep != service.OtherDep0);
+            Console.WriteLine(service.Dep != service.OtherDep1);                                         
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.StdOut.ShouldBe(ImmutableArray.Create("True", "True"), result.GeneratedCode);
+    }
+}
