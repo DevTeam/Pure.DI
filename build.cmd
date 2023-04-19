4eaa1c1d5b02a78d4001ea47230cdc80ba21df4c
NikolayPianikov <NikolayP@mail.ru>
2023-04-19 23:33:03 +0300

    Update cmd files

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-%DOTNET_ROOT%/dotnet.exe run --project ./build -- %1
+dotnet.exe run --project ./build -- "%1"
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$DOTNET_ROOT/dotnet run --project ./build -- $1
+dotnet run --project ./build -- "$1"
