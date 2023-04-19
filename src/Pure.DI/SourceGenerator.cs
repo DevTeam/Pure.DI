9408171e7b8992aef060f762db53eee5bf30d7e3
Nikolay Pyanikov <n.pyanikov@tinkoff.ru>
2023-04-19 12:27:21 +0300

    Cancels a code generation when other one is required

--- src/Pure.DI/SourceGenerator.cs
+++ src/Pure.DI/SourceGenerator.cs
@@ -3,6 +3,9 @@ namespace Pure.DI;
 [Generator(LanguageNames.CSharp)]
 public class SourceGenerator: IIncrementalGenerator
 {
+    private static readonly object LockObject = new();
+    private static CancellationTokenSource? _cancellationTokenSource;
+    
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
         // ReSharper disable once InvocationIsSkipped
@@ -25,6 +28,17 @@ public class SourceGenerator: IIncrementalGenerator
 
         context.RegisterSourceOutput(valuesProvider, (sourceProductionContext, options) =>
         {
+            var cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(sourceProductionContext.CancellationToken);
+            lock (LockObject)
+            {
+                if (_cancellationTokenSource is not null)
+                {
+                    _cancellationTokenSource.Cancel();
+                    _cancellationTokenSource.Dispose();
+                    _cancellationTokenSource = cancellationTokenSource;
+                }
+            }
+
             var changes = options.Right;
             if (changes.Length == 0)
             {
@@ -36,7 +50,7 @@ public class SourceGenerator: IIncrementalGenerator
             var updates = changes.Select(change => new SyntaxUpdate(change.Node, change.SemanticModel));
             var ctx = new ContextInitializer(sourceProductionContext, parseOptions, analyzerConfigOptionsProvider);
             var facade = Facade.Create(ctx, ctx, ctx);
-            facade.Generator.Generate(updates, sourceProductionContext.CancellationToken);
+            facade.Generator.Generate(updates,cancellationTokenSource.Token);
         });
     }
 }
