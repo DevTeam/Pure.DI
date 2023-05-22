9868d7e11e72a9fca8353022bbeb84103c232312
NikolayPianikov <NikolayP@mail.ru>
2023-05-22 22:44:22 +0300

    Simplifying the WPF Example - Dispose composition

---
+++ samples/WpfAppNetCore/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WpfAppNetCore;
+
+using System.Windows;
+
+internal partial class App
+{
+    private void OnExit(object sender, ExitEventArgs e) => 
+        (TryFindResource("Composition") as IDisposable)?.Dispose();
+}
