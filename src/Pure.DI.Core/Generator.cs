e9487cf3319fa31fbb1b0e9529df2aa5735e5203
Nikolay Pyanikov <n.pyanikov@tinkoff.ru>
2023-08-01 13:11:57 +0300

    Optimizing internal Composition

--- src/Pure.DI.Core/Generator.cs
+++ src/Pure.DI.Core/Generator.cs
@@ -8,8 +8,8 @@ public class Generator
     public static IEnumerable<Source> GetApi(CancellationToken cancellationToken) => 
         CompositionBase.ApiBuilder.Build(Unit.Shared, cancellationToken);
 
-    public Generator(IOptions options, ISourcesRegistry sourcesRegistry, IDiagnostic diagnostic) =>
-        _composition = new Composition(options: options, producer: sourcesRegistry, diagnostic: diagnostic);
+    public Generator(IOptions options, ISourcesRegistry sources, IDiagnostic diagnostic) =>
+        _composition = new Composition(options: options, sources: sources, diagnostic: diagnostic);
 
     public void Generate(
         IEnumerable<SyntaxUpdate> updates,
