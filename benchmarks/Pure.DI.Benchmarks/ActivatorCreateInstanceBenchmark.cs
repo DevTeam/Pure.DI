3aade8fd2645558bf35c7cdd7d894c4244130440
Nikolay Pyanikov <n.pyanikov@tinkoff.ru>
2023-08-06 13:30:22 +0300

    Adding benchmarks for Activator.CreateInstance

---
+++ benchmarks/Pure.DI.Benchmarks/ActivatorCreateInstanceBenchmark.cs
@@ -0,0 +1,45 @@
+namespace Pure.DI.Benchmarks;
+
+using System.Diagnostics.CodeAnalysis;
+using BenchmarkDotNet.Attributes;
+using Model;
+
+[SuppressMessage("Performance", "CA1822:Mark members as static")]
+public class ActivatorCreateInstanceBenchmark
+{
+    [Benchmark(Description = "Activator.CreateInstance")]
+    public CompositionRoot ActivatorCreateInstance()
+    {
+        return CreateInstance<CompositionRoot>(
+            CreateInstance<Service1>(
+                CreateInstance<Service2>(
+                    CreateInstance<Service3>(),
+                    CreateInstance<Service3>(),
+                    CreateInstance<Service3>(),
+                    CreateInstance<Service3>(),
+                    CreateInstance<Service3>())),
+            CreateInstance<Service2>(
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>()),
+            CreateInstance<Service2>(
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>()),
+            CreateInstance<Service2>(
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>(),
+                CreateInstance<Service3>()),
+            CreateInstance<Service3>());
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static T CreateInstance<T>(params object[] args) => 
+        (T)Activator.CreateInstance(typeof(T), args)!;
+}
