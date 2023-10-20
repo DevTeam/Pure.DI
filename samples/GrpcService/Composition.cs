56f4c85db1813891dd7bd56638e04035c432ad1f
Nikolay Pianikov <nikolayp@mail.ru>
2023-10-20 16:32:23 +0300

    Add GrpcService sample

---
+++ samples/GrpcService/Composition.cs
@@ -0,0 +1,15 @@
+// ReSharper disable UnusedMember.Local
+
+namespace GrpcService;
+
+using Pure.DI;
+using Pure.DI.MS;
+using Services;
+
+internal partial class Composition: ServiceProviderFactory<Composition>
+{
+    private static void Setup() =>
+        DI.Setup(nameof(Composition))
+            .DependsOn(Base)
+            .Root<GreeterService>();
+}
---
+++ samples/GrpcService/Program.cs
@@ -0,0 +1,21 @@
+using GrpcService;
+using GrpcService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+// Uses Composition as an alternative IServiceProviderFactory
+builder.Host.UseServiceProviderFactory(new Composition());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
