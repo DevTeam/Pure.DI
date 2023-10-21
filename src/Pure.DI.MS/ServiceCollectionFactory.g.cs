3e4623e48c5f9bffd38863f709d8b8e69cd606d8
Nikolay Pianikov <nikolayp@mail.ru>
2023-10-21 14:56:35 +0300

    Adds docs for ServiceCollectionFactory

--- src/Pure.DI.MS/ServiceCollectionFactory.g.cs
+++ src/Pure.DI.MS/ServiceCollectionFactory.g.cs
@@ -6,25 +6,32 @@ namespace Pure.DI.MS;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
+/// <summary>
+/// Creates a service collection <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/> based resolvers.
+/// </summary>
+/// <typeparam name="TComposition">The composition class itself.</typeparam>
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 internal class ServiceCollectionFactory<TComposition>
 {
+    /// <summary>
+    /// A list of instance resolvers, specifying the type of object that the resolver returns.
+    /// </summary>
     private readonly List<(Type ServiceType, IResolver<TComposition, object> Resolver)> _resolvers = new();
     
     /// <summary>
-    /// Registers a resolver of the composition for use in a service collection.
+    /// Registers the resolver of a composition for use in a collection of services.
     /// </summary>
-    /// <param name="resolver">Composition resolver.</param>
+    /// <param name="resolver">Instance resolver.</param>
     /// <typeparam name="TContract">The type of object that the resolver returns.</typeparam>
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     public void AddResolver<TContract>(IResolver<TComposition, TContract> resolver) => 
         _resolvers.Add((typeof(TContract), (IResolver<TComposition, object>)resolver));
 
     /// <summary>
-    /// Creates a service collection.
+    /// Creates a service collection <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/> based on all previously registered resolvers.
     /// </summary>
     /// <param name="composition">An instance of composition.</param>
-    /// <returns><see cref="ServiceCollection"/></returns>
+    /// <returns>An instance of <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/>.</returns>
 #if NETSTANDARD2_0_OR_GREATER || NETCOREAPP || NET40_OR_GREATER
     [global::System.Diagnostics.Contracts.Pure]
 #endif
@@ -32,10 +39,22 @@ internal class ServiceCollectionFactory<TComposition>
     public IServiceCollection CreateServiceCollection(TComposition composition) => 
         new ServiceCollection().Add(CreateDescriptors(composition));
 
+    /// <summary>
+    /// Creates an enumeration of service descriptors based on all previously registered resolvers.
+    /// </summary>
+    /// <param name="composition">An instance of composition.</param>
+    /// <returns>A enumeration of <see cref="Microsoft.Extensions.DependencyInjection.ServiceDescriptor"/>.</returns>
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     private IEnumerable<ServiceDescriptor> CreateDescriptors(TComposition composition) =>
         _resolvers.Select(resolver => CreateDescriptor(composition, resolver.ServiceType, resolver.Resolver));
 
+    /// <summary>
+    /// Creates a service descriptor for the passed resolver.
+    /// </summary>
+    /// <param name="composition">An instance of composition.</param>
+    /// <param name="serviceType">The type of object that the resolver returns.</param>
+    /// <param name="resolver">Instance resolver.</param>
+    /// <returns>An instance of <see cref="Microsoft.Extensions.DependencyInjection.ServiceDescriptor"/>.</returns>
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     private static ServiceDescriptor CreateDescriptor(TComposition composition, Type serviceType, IResolver<TComposition, object> resolver) =>
         new(serviceType, _ => resolver.Resolve(composition), ServiceLifetime.Transient);
--- src/Pure.DI.MS/ServiceProviderFactory.g.cs
+++ src/Pure.DI.MS/ServiceProviderFactory.g.cs
@@ -6,14 +6,51 @@ namespace Pure.DI.MS;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
+/// <summary>
+/// A base class for a composition that can be used as a service provider factory <see cref="Microsoft.Extensions.DependencyInjection.IServiceProviderFactory{TContainerBuilder}"/>.
+/// <example>
+/// For example:
+/// <code>
+/// internal partial class Composition: ServiceProviderFactory&lt;Composition&gt;
+/// {
+///     private static void Setup() =&gt;
+///         DI.Setup(nameof(Composition))
+///             .DependsOn(Base)
+///             .Root&lt;HomeController&gt;();
+/// }
+/// </code>
+/// </example> 
+/// </summary>
+/// <typeparam name="TComposition"></typeparam>
 [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 internal class ServiceProviderFactory<TComposition>: IServiceProviderFactory<IServiceCollection>
     where TComposition: ServiceProviderFactory<TComposition>
 {
+    /// <summary>
+    /// The name of the Pure.DI setup to use as a dependency in other setups.
+    /// <example>
+    /// For example:
+    /// <code>
+    /// private static void Setup() =&amp;gt;
+    ///     DI.Setup(nameof(Composition)).DependsOn(Base);
+    /// </code>
+    /// </example>
+    /// </summary>
     protected const string Base = $"Pure.DI.MS.ServiceProviderFactory";
+    
+    /// <summary>
+    /// An instance of <see cref="Pure.DI.MS.ServiceCollectionFactory"/>.
+    /// </summary>
     private static readonly ServiceCollectionFactory<TComposition> ServiceCollectionFactory = new();
+    
+    /// <summary>
+    /// <see cref="System.IServiceProvider"/> instance for resolving external dependencies.
+    /// </summary>
     private volatile IServiceProvider? _serviceProvider;
     
+    /// <summary>
+    /// DI setup hints.
+    /// </summary>
     [global::System.Diagnostics.Conditional("A2768DE22DE3E430C9653990D516CC9B")]
     private static void HintsSetup() =>
         DI.Setup(Base, CompositionKind.Internal)
@@ -22,6 +59,11 @@ internal class ServiceProviderFactory<TComposition>: IServiceProviderFactory<ISe
             .Hint(Hint.OnNewRoot, "On")
             .Hint(Hint.OnNewRootPartial, "Off");
 
+    /// <summary>
+    /// Creates a service collection <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/> based on the registered composition.
+    /// </summary>
+    /// <param name="composition">An instance of composition.</param>
+    /// <returns>An instance of <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/>.</returns>
 #if NETSTANDARD2_0_OR_GREATER || NETCOREAPP || NET40_OR_GREATER
     [global::System.Diagnostics.Contracts.Pure]
 #endif
@@ -32,7 +74,7 @@ internal class ServiceProviderFactory<TComposition>: IServiceProviderFactory<ISe
     /// <inheritdoc />
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     public IServiceCollection CreateBuilder(IServiceCollection services) =>
-        // Registers composition roots as services in the service collection
+        // Registers composition roots as services in the service collection.
         services.Add(CreateServiceCollection((TComposition)this));
 
     /// <inheritdoc />
@@ -40,18 +82,31 @@ internal class ServiceProviderFactory<TComposition>: IServiceProviderFactory<ISe
     public IServiceProvider CreateServiceProvider(IServiceCollection services)
     {
         var serviceProvider = services.BuildServiceProvider();
-        // Saves the service provider to use it to resolve dependencies external
-        // to this composition from the service provider 
+        // Saves the service provider to use it to resolve dependencies external to this composition from the service provider.
         _serviceProvider ??= services.BuildServiceProvider();
         return serviceProvider;
     }
 
-    // Obtaining external dependencies from the service provider
+    /// <summary>
+    /// Used to resolve external dependencies using the service provider <see cref="System.IServiceProvider"/>.
+    /// </summary>
+    /// <param name="tag">Dependency resolution tag.</param>
+    /// <param name="lifetime">Dependency resolution lifetime.</param>
+    /// <typeparam name="T">Dependency resolution type.</typeparam>
+    /// <returns>Resolved dependency instance.</returns>
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     protected T OnCannotResolve<T>(object? tag, Lifetime lifetime) =>
         _serviceProvider.GetRequiredService<T>();
-
-    // Registers the composition resolvers for use in a service collection
+    
+    /// <summary>
+    /// Registers a composition resolver for use in a service collection <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/>.
+    /// </summary>
+    /// <param name="resolver">Instance resolver.</param>
+    /// <param name="name">The name of the composition root.</param>
+    /// <param name="tag">The tag of the composition root.</param>
+    /// <param name="lifetime">The lifetime of the composition root.</param>
+    /// <typeparam name="TContract">The contract type of the composition root.</typeparam>
+    /// <typeparam name="T">The implementation type of the composition root.</typeparam>
     [global::System.Runtime.CompilerServices.MethodImpl((global::System.Runtime.CompilerServices.MethodImplOptions)0x300)]
     protected static void OnNewRoot<TContract, T>(
         IResolver<TComposition, TContract> resolver,
