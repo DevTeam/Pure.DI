0ee775d35fb8d917660bcb4100d2e25d27dfe50a
Nikolay Pianikov <nikolayp@mail.ru>
2023-10-31 18:40:49 +0300

    Validates names

--- src/Pure.DI.Core/Core/MetadataValidator.cs
+++ src/Pure.DI.Core/Core/MetadataValidator.cs
@@ -24,6 +24,23 @@ internal sealed class MetadataValidator : IValidator<MdSetup>
         {
             return false;
         }
+        
+        if (setup.Kind == CompositionKind.Public
+            && (!SyntaxFacts.IsValidIdentifier(setup.Name.ClassName)
+                || !IsValidOrEmptyIdentifier(setup.Name.Namespace.Replace('.', '_'))))
+        {
+            _logger.CompileError($"Invalid composition type name \"{setup.Name}\".", setup.Source.GetLocation(), LogId.ErrorInvalidMetadata);
+            isValid = false;
+        }
+
+        foreach (var root in setup.Roots)
+        {
+            if (!IsValidOrEmptyIdentifier(root.Name))
+            {
+                _logger.CompileError($"Invalid root name \"{root.Name}\".", root.Source.GetLocation(), LogId.ErrorInvalidMetadata);
+                isValid = false;
+            }
+        }
 
         foreach (var routeGroups in setup.Roots.GroupBy(root => new Injection(root.RootType, root.Tag?.Value)))
         {
@@ -48,9 +65,14 @@ internal sealed class MetadataValidator : IValidator<MdSetup>
         return true;
     }
 
+    private static bool IsValidOrEmptyIdentifier(string identifier) => 
+        string.IsNullOrEmpty(identifier)
+        || SyntaxFacts.IsValidIdentifier(identifier);
+
     [SuppressMessage("MicrosoftCodeAnalysisCorrectness", "RS1024:Symbols should be compared for equality")]
     private bool Validate(in MdBinding binding)
     {
+        var isValid = true;
         ITypeSymbol? implementationType = default;
         SemanticModel? semanticModel = default;
         var location = binding.Source.GetLocation();
@@ -75,17 +97,18 @@ internal sealed class MetadataValidator : IValidator<MdSetup>
                     semanticModel = arg.SemanticModel;
                     implementationType = arg.Type;
                     location = arg.Source.GetLocation();
+                    if (!SyntaxFacts.IsValidIdentifier(arg.ArgName))
+                    {
+                        _logger.CompileError($"Invalid argument name \"{arg.ArgName}\".", location, LogId.ErrorInvalidMetadata);
+                        isValid = false;
+                    }
                 }
             }
         }
 
-        if (implementationType == default || semanticModel == default)
+        if (implementationType == default || implementationType is IErrorTypeSymbol || semanticModel == default)
         {
-            throw new CompileErrorException("Invalid binding due to construction failure.", location, LogId.ErrorInvalidMetadata);
-        }
-        
-        if (implementationType is IErrorTypeSymbol)
-        {
+            _logger.CompileError("Invalid binding due to construction failure.", location, LogId.ErrorInvalidMetadata);
             return false;
         }
         
@@ -94,14 +117,14 @@ internal sealed class MetadataValidator : IValidator<MdSetup>
             .Where(contract => !supportedContracts.Contains(contract.ContractType))
             .Select(i => i.ContractType)
             .ToArray();
-        
-        if (!notSupportedContracts.Any())
+
+        if (notSupportedContracts.Any())
         {
-            return true;
+            _logger.CompileError($"{implementationType} does not implement {string.Join(", ", notSupportedContracts.Select(i => i.ToString()))}.", location, LogId.ErrorInvalidMetadata);
+            isValid = false;
         }
 
-        _logger.CompileError($"{implementationType} does not implement {string.Join(", ", notSupportedContracts.Select(i => i.ToString()))}.", location, LogId.ErrorInvalidMetadata);
-        return false;
+        return isValid;
     }
     
     private static IEnumerable<ITypeSymbol> GetBaseTypes(ITypeSymbol symbol)
