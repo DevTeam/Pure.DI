a9f6a76522e6bebaa0dbe437571dee7925892d00
Nikolay Pianikov <nikolayp@mail.ru>
2024-01-12 12:52:29 +0300

    Minor

--- src/Pure.DI.Core/Core/LinesBuilder.cs
+++ src/Pure.DI.Core/Core/LinesBuilder.cs
@@ -26,19 +26,18 @@ internal sealed class LinesBuilder: IEnumerable<string>
     {
         get
         {
-            if (_sb.Length > 0)
-            {
-                AppendLine();
-            }
-
+            FlushLines();
             return _lines;
         }
     }
 
     public void Append(string text) => _sb.Append(text);
 
-    public void AppendLine(in Line line) => 
+    public void AppendLine(in Line line)
+    {
+        FlushLines();
         _lines.Add(line with { Indent = line.Indent + _indent.Value });
+    }
 
     public void AppendLines(IEnumerable<Line> lines)
     {
@@ -64,25 +63,38 @@ internal sealed class LinesBuilder: IEnumerable<string>
 
     public IDisposable Indent(int value = 1)
     {
+        
         IncIndent(value);
         return Disposables.Create(() => DecIndent(value));
     }
-    
-    public void IncIndent(int value = 1) => _indent.Value += value;
-    
-    public void DecIndent(int value = 1) => _indent.Value -= value;
+
+    public void IncIndent(int value = 1)
+    {
+        FlushLines();
+        _indent.Value += value;
+    }
+
+    public void DecIndent(int value = 1)
+    {
+        FlushLines();
+        _indent.Value -= value;
+    }
 
     public IEnumerator<string> GetEnumerator()
+    {
+        FlushLines();
+        return _lines.Select(i => $"{GetIndent(i.Indent)}{i.Text}").GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private void FlushLines()
     {
         if (_sb.Length > 0)
         {
             AppendLine();
         }
-
-        return _lines.Select(i => $"{GetIndent(i.Indent)}{i.Text}").GetEnumerator();
     }
-
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     private static string GetIndent(int indent)
     {
