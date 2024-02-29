47eb488c6f4b241de626c5cfe446d060dcbd3e3d
Nikolay Pianikov <nikolayp@mail.ru>
2024-02-29 18:53:34 +0300

    Fixes bug when IServiceProvider works as IRequiredServiceProvider and should return null if there is no way to resolve the dependency.
    Fixes a bug where a "unable to resolve dependencies" error appears for types that are excluded from checking.
    
    Adds hints to examples on how not to exclude all types other than excluded types from dependency getability checks.

--- samples/BlazorWebAssemblyApp/Composition.cs
+++ samples/BlazorWebAssemblyApp/Composition.cs
@@ -14,6 +14,10 @@ internal partial class Composition: ServiceProviderFactory<Composition>
     private static void Setup() =>
         DI.Setup(nameof(Composition))
             .DependsOn(Base)
+            // Specifies not to attempt to resolve types whose fully qualified name
+            // begins with Microsoft.Extensions., Microsoft.AspNetCore.
+            // since ServiceProvider will be used to retrieve them.
+            .Hint(Hint.OnCannotResolveContractTypeNameRegularExpression, "^Microsoft\\.(Extensions|AspNetCore)\\..+$")
             
             // View Models
             .Bind<IClockViewModel>()
--- samples/MinimalWebAPI/Composition.cs
+++ samples/MinimalWebAPI/Composition.cs
@@ -12,9 +12,15 @@ internal partial class Composition: ServiceProviderFactory<Composition>
     private static void Setup() =>
         DI.Setup(nameof(Composition))
             .DependsOn(Base)
+            // Specifies not to attempt to resolve types whose fully qualified name
+            // begins with Microsoft.Extensions., Microsoft.AspNetCore.
+            // since ServiceProvider will be used to retrieve them.
+            .Hint(Hint.OnCannotResolveContractTypeNameRegularExpression, "^Microsoft\\.(Extensions|AspNetCore)\\..+$")
+            
             .Bind<IWeatherForecastService>()
                 .As(Singleton)
                 .To<WeatherForecastService>()
+            
             // Application composition root
             .Root<Program>("Root");
 }
