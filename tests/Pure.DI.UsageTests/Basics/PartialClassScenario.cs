f3137d0094c723bbecfd8934f92e67d121db40c5
Nikolay Pianikov <nikolayp@mail.ru>
2024-03-05 11:39:47 +0300

    Improved example for Partial class

--- tests/Pure.DI.UsageTests/Basics/PartialClassScenario.cs
+++ tests/Pure.DI.UsageTests/Basics/PartialClassScenario.cs
@@ -19,23 +19,54 @@ using Shouldly;
 using Xunit;
 
 // {
-interface IDependency;
+interface IDependency
+{
+    long Id { get; }
+}
 
-class Dependency : IDependency;
+class Dependency(long id) : IDependency
+{
+    public long Id { get; } = id;
+}
 
-interface IService;
+class Service(
+    [Tag("name with id")] string name,
+    IDependency dependency1,
+    IDependency dependency2)
+{
+    public string Name { get; } = name;
 
-class Service(IDependency dependency) : IService;
+    public IDependency Dependency1 { get; } = dependency1;
+    
+    public IDependency Dependency2 { get; } = dependency2;
+}
 
 // The partial class is also useful for specifying access modifiers to the generated class
-partial class Composition
+public partial class Composition
 {
-    // This method will not be called in runtime
+    private readonly string _serviceName;
+    private long _id;
+
+    // Customizable constructor
+    public Composition(string serviceName)
+        : this()
+    {
+        _serviceName = serviceName;
+    }
+
+    private long GenerateId() => Interlocked.Increment(ref _id);
+    
+    // In fact, this method will not be called at runtime
     [Conditional("DI")]
     private void Setup() =>
         DI.Setup(nameof(Composition))
             .Bind<IDependency>().To<Dependency>()
-            .Bind<IService>().To<Service>().Root<IService>("Root");
+            .Bind<long>().To(_ => GenerateId())
+            .Bind<string>("name with id").To(
+                _ => $"{_serviceName}_{GenerateId()}")
+            .Root<Service>(
+                "Root",
+                kind: RootKinds.Internal);
 }
 // }
 
@@ -45,8 +76,12 @@ public class Scenario
     public void Run()
     {
 // {            
-        var composition = new Composition();
+        var composition = new Composition("Abc");
         var service = composition.Root;
+        
+        service.Dependency1.Id.ShouldBe(1);
+        service.Dependency2.Id.ShouldBe(2);
+        service.Name.ShouldBe("Abc_3");
 // }            
         service.ShouldBeOfType<Service>();
         composition.SaveClassDiagram();
