0d502fb0c65744e5761a9e02ce27d5f6aaa84328
Nikolay Pianikov <nikolayp@mail.ru>
2024-03-22 17:41:35 +0300

    Minor

--- samples/Clock/Models/Log.cs
+++ samples/Clock/Models/Log.cs
@@ -7,5 +7,5 @@ using System.Diagnostics;
 internal class Log<T>(IClock clock) : ILog<T>
 {
     public void Info(string message) =>
-        Debug.WriteLine($"{clock.Now:HH:mm:ss.fff} {typeof(T).Name,-64} {message}");
+        Debug.WriteLine($"{clock.Now:HH:mm:ss.fff} {typeof(T).Name,-32} {message}");
 }
