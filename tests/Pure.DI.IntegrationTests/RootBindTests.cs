c1569641e4e0e02851737b1b9cb7c45a9b741f3d
Serg046 <serg046@outlook.com>
2024-03-23 22:44:55 +0200

    Issue #41: Ability to register composition roots with fewer API calls

---
+++ tests/Pure.DI.IntegrationTests/RootBindTests.cs
@@ -0,0 +1,226 @@
+﻿namespace Pure.DI.IntegrationTests;
+
+public class RootBindTests
+{
+    [Fact]
+    public async Task ShouldSupportRootBind()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using System.Collections.Generic;
+using Pure.DI;
+
+namespace Sample
+{
+    interface IDependency {}
+
+    class Dependency: IDependency {}
+
+    static class Setup
+    {
+        private static void SetupComposition()
+        {
+            DI.Setup("Composition")
+                .RootBind<IDependency>().To<Dependency>();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composition = new Composition();
+            var service = composition.Resolve<IDependency>();
+            Console.WriteLine(service.GetType() == typeof(Dependency));
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["True"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportRootBindWithRootName()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using System.Collections.Generic;
+using Pure.DI;
+
+namespace Sample
+{
+    interface IDependency {}
+
+    class Dependency: IDependency {}
+
+    static class Setup
+    {
+        private static void SetupComposition()
+        {
+            DI.Setup("Composition")
+                .RootBind<IDependency>("Root").To<Dependency>();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composition = new Composition();
+            var service = composition.Root;
+            Console.WriteLine(service.GetType() == typeof(Dependency));
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["True"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportRootBindWithRootKind()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using System.Collections.Generic;
+using Pure.DI;
+
+namespace Sample
+{
+    interface IDependency {}
+
+    class Dependency: IDependency {}
+
+    static class Setup
+    {
+        private static void SetupComposition()
+        {
+            DI.Setup("Composition")
+                .RootBind<IDependency>("Root", RootKinds.Method).To<Dependency>();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composition = new Composition();
+            var service = composition.Root();
+            Console.WriteLine(service.GetType() == typeof(Dependency));
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["True"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportRootBindWithTags()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using System.Collections.Generic;
+using Pure.DI;
+using static Pure.DI.Lifetime;
+
+namespace Sample
+{
+    interface IDependency {}
+
+    class Dependency: IDependency {}
+
+    static class Setup
+    {
+        private static void SetupComposition()
+        {
+            DI.Setup("Composition")
+                .Bind<IDependency>().To<Dependency>()
+                .RootBind<IDependency>("Root", RootKinds.Property, "RootTag", "Dep2").As(Singleton).To<Dependency>()
+                .Root<IDependency>("Root2", "Dep2")
+                .Root<IDependency>("Root3");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composition = new Composition();
+            Console.WriteLine(composition.Root == composition.Root2);
+            Console.WriteLine(composition.Root != composition.Root3);
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["True", "True"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportRootBindWithTagsAsNamedArgument()
+    {
+        // Given
+
+        // When
+        var result = await """
+using System;
+using System.Collections.Generic;
+using Pure.DI;
+using static Pure.DI.Lifetime;
+
+namespace Sample
+{
+    interface IDependency {}
+
+    class Dependency: IDependency {}
+
+    static class Setup
+    {
+        private static void SetupComposition()
+        {
+            DI.Setup("Composition")
+                .Bind<IDependency>().To<Dependency>()
+                .RootBind<IDependency>(tags: "RootTag").As(Singleton).To<Dependency>()
+                .Root<IDependency>("Root2");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var composition = new Composition();
+            Console.WriteLine(composition.Resolve<IDependency>("RootTag") == composition.Resolve<IDependency>("RootTag"));
+            Console.WriteLine(composition.Resolve<IDependency>("RootTag") != composition.Root2);
+        }
+    }                
+}
+""".RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["True", "True"], result);
+    }
+}
---
+++ tests/Pure.DI.UsageTests/Basics/RootBindScenario.cs
@@ -0,0 +1,37 @@
+﻿/*
+$v=true
+$p=17
+$d=RootBind
+$h=You might want to register some services as roots. You can use `RootBind<T>()` method in order to reduce repetitions. The registration `composition.RootBind<IDependency>().To<Dependency>()` is an equivalent to `composition.Bind<IDependency>().To<Dependency>().Root<IDependency>()`.
+*/
+
+// ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable ArrangeTypeModifiers
+namespace Pure.DI.UsageTests.Basics.RootBindScenario;
+
+using Shouldly;
+using Xunit;
+
+// {
+interface IDependency;
+
+class Dependency : IDependency;
+// }
+
+public class Scenario
+{
+    [Fact]
+    public void Run()
+    {
+        // {            
+        DI.Setup(nameof(Composition))
+            .RootBind<IDependency>("Root").To<Dependency>();
+
+        var composition = new Composition();
+        composition.Root.ShouldBeOfType<Dependency>();
+        // }
+        composition.SaveClassDiagram();
+    }
+}
