96f26d952c0bf92062dc4ba12bf358b8ac7cb4f0
Nikolay Pianikov <nikolayp@mail.ru>
2024-08-22 11:18:55 +0300

    Adds TTE as a generic type marker for enumerations

--- src/Pure.DI.Core/Components/Api.g.cs
+++ src/Pure.DI.Core/Components/Api.g.cs
@@ -810,7 +810,7 @@ namespace Pure.DI
     /// </code>
     /// </example>
     /// </summary>
-    [global::System.AttributeUsage(global::System.AttributeTargets.Class | global::System.AttributeTargets.Interface | global::System.AttributeTargets.Struct)]
+    [global::System.AttributeUsage(global::System.AttributeTargets.Class | global::System.AttributeTargets.Interface | global::System.AttributeTargets.Struct | global::System.AttributeTargets.Enum)]
 #if !NET20 && !NET35 && !NETSTANDARD1_0 && !NETSTANDARD1_1 && !NETSTANDARD1_2 && !NETSTANDARD1_3 && !NETSTANDARD1_4 && !NETSTANDARD1_5 && !NETSTANDARD1_6 && !NETCOREAPP1_0 && !NETCOREAPP1_1
     [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 #endif
