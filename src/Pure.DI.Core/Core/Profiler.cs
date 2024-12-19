e970e39a44031ce9cdfe6cf30545ccf81e9937f1
Nikolay Pianikov <nikolayp@mail.ru>
2024-12-19 10:14:24 +0300

    Add profiler warnings

--- src/Pure.DI.Core/Core/Profiler.cs
+++ src/Pure.DI.Core/Core/Profiler.cs
@@ -3,17 +3,42 @@
 namespace Pure.DI.Core;
 
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 
-internal class Profiler(CancellationToken cancellationToken) : IProfiler
+internal class Profiler(ILogger<Profiler> logger, CancellationToken cancellationToken) : IProfiler
 {
     private CancellationToken _cancellationToken = cancellationToken;
 
     public void Profiling(string path)
     {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            logger.Log(
+                new LogEntry
+                {
+                    Message = "Profiling is only supported on Windows.",
+                    Severity = DiagnosticSeverity.Warning
+                });
+            
+            return;
+        }
+
         Directory.CreateDirectory(path);
         var traceFile = Path.Combine(path, $"pure_di_{Guid.NewGuid().ToString()[..4]}.dtt");
         var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
         var profiler = Path.Combine(userProfile, ".dotnet", "tools", "dottrace.exe");
+        if (!File.Exists(profiler))
+        {
+            logger.Log(
+                new LogEntry
+                {
+                    Message = $"Unable to find the \"{profiler}\" profiler. Please install the dotnet tool \"https://www.nuget.org/packages/JetBrains.dotTrace.GlobalTools\" globally.",
+                    Severity = DiagnosticSeverity.Warning
+                });
+            
+            return;
+        }
+
         var traceProcess = new Process
         {
             StartInfo = new ProcessStartInfo(
