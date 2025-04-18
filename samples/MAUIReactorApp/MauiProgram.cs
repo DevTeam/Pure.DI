af9f5c4b93dfb29792db073c6084518fc7301c22
Nikolay Pianikov <nikolayp@mail.ru>
2025-04-18 22:12:31 +0300

    MAUI Reactor App sample

---
+++ samples/MAUIReactorApp/MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿namespace MAUIReactorApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        using var composition = new Composition();
+        // Uses Composition as an alternative IServiceProviderFactory
+        builder.ConfigureContainer(composition);
+
+        builder
+            .UseMauiApp<App>()
+            // or .UseMauiApp(serviceProvider => new App(serviceProvider))
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
