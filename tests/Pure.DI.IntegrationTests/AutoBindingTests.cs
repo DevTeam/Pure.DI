5035d4e9cb0852559967ced09d6f83edb6b0f471
Nikolay Pianikov <nikolayp@mail.ru>
2025-12-21 18:08:45 +0300

    Add integration tests for various auto-binding scenarios, including nested dependencies, records, generic types, primary constructors, constructor ordinal attributes, and unsupported cases

--- tests/Pure.DI.IntegrationTests/AutoBindingTests.cs
+++ tests/Pure.DI.IntegrationTests/AutoBindingTests.cs
@@ -115,4 +115,316 @@ public class AutoBindingTests
         result.Success.ShouldBeTrue(result);
         result.StdOut.ShouldBe(["Ctor1"], result);
     }
+
+    [Fact]
+    public async Task ShouldSupportNestedAutoBinding()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               class Dependency2
+                               {
+                                   public Dependency2()
+                                   {
+                                       Console.WriteLine("CtorDep2");
+                                   }
+                               }
+
+                               class Dependency1
+                               {
+                                   public Dependency1(Dependency2 dep2)
+                                   {
+                                       Console.WriteLine("CtorDep1");
+                                   }
+                               }
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(Dependency1 dep1)
+                                   {
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           
+                               public class Program
+                               {
+                                   public static void Main()
+                                   {
+                                       var service = new Composition().Service;         
+                                   }
+                               }
+                           }
+                           """.RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["CtorDep2", "CtorDep1"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportAutoBindingForRecord()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               public record Dependency(string Name = "Default");
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(Dependency dep)
+                                   {
+                                       Console.WriteLine(dep.Name);
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           
+                               public class Program
+                               {
+                                   public static void Main()
+                                   {
+                                       var service = new Composition().Service;         
+                                   }
+                               }
+                           }
+                           """.RunAsync(new Options { LanguageVersion = LanguageVersion.CSharp12 });
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["Default"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportAutoBindingForClassWithPrimaryConstructor()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               public class Dependency(int id = 10)
+                               {
+                                   public int Id => id;
+                               }
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(Dependency dep)
+                                   {
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           
+                               public class Program
+                               {
+                                   public static void Main()
+                                   {
+                                       var service = new Composition().Service;         
+                                   }
+                               }
+                           }
+                           """.RunAsync(new Options { LanguageVersion = LanguageVersion.CSharp12 });
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+    }
+
+    [Fact]
+    public async Task ShouldNotSupportAutoBindingForInterface()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               interface IDependency {}
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(IDependency dep)
+                                   {
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           }
+                           """.RunAsync();
+
+        // Then
+        result.Success.ShouldBeFalse();
+        result.Errors.Count.ShouldBeGreaterThan(0);
+    }
+
+    [Fact]
+    public async Task ShouldSupportAutoBindingForGenericType()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               class Dependency<T>
+                               {
+                                   public Dependency()
+                                   {
+                                       Console.WriteLine(typeof(T).Name);
+                                   }
+                               }
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(Dependency<int> dep)
+                                   {
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           
+                               public class Program
+                               {
+                                   public static void Main()
+                                   {
+                                       var service = new Composition().Service;         
+                                   }
+                               }
+                           }
+                           """.RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["Int32"], result);
+    }
+
+    [Fact]
+    public async Task ShouldSupportAutoBindingWhenConstructorOrdinalAttribute()
+    {
+        // Given
+
+        // When
+        var result = await """
+                           using System;
+                           using Pure.DI;
+
+                           namespace Sample
+                           {
+                               class Dependency1 {}
+                               class Dependency2 {}
+                               class Dependency
+                               {
+                                   [Ordinal(1)]
+                                   public Dependency(Dependency1 d1)
+                                   {
+                                       Console.WriteLine("Ctor1");
+                                   }
+
+                                   [Ordinal(0)]
+                                   public Dependency(Dependency2 d2)
+                                   {
+                                       Console.WriteLine("Ctor2");
+                                   }
+                               }
+                           
+                               interface IService {}
+                               class Service: IService
+                               {
+                                   public Service(Dependency dep)
+                                   {
+                                   }
+                               }
+                           
+                               static class Setup
+                               {
+                                   private static void SetupComposition()
+                                   {
+                                       DI.Setup("Composition")
+                                           .Bind<IService>().As(Lifetime.Transient).To<Service>()
+                                           .Root<IService>("Service");
+                                   }
+                               }
+                           
+                               public class Program
+                               {
+                                   public static void Main()
+                                   {
+                                       var service = new Composition().Service;         
+                                   }
+                               }
+                           }
+                           """.RunAsync();
+
+        // Then
+        result.Success.ShouldBeTrue(result);
+        result.StdOut.ShouldBe(["Ctor2"], result);
+    }
 }
