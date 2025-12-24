c080aafd6c21cf02a9debe86466cdcb821872c8a
Nikolay Pianikov <nikolayp@mail.ru>
2025-12-24 11:25:21 +0300

    Refactor `Constructors` to include scoped constructor enablement logic

--- src/Pure.DI.Core/Core/Code/Constructors.cs
+++ src/Pure.DI.Core/Core/Code/Constructors.cs
@@ -1,13 +1,26 @@
 ﻿namespace Pure.DI.Core.Code;
 
-class Constructors : IConstructors
+class Constructors(
+    IRegistry<int> registry)
+    : IConstructors
 {
     public bool IsEnabled(CompositionCode composition, ConstructorKind kind) =>
         kind switch
         {
             ConstructorKind.Default => composition.ClassArgs.Length == 0,
             ConstructorKind.Parameterized => composition.ClassArgs.Length > 0,
-            ConstructorKind.Scope => true,
+            ConstructorKind.Scope => IsScopedConstructorEnabled(composition),
             _ => throw new ArgumentOutOfRangeException(nameof(kind), kind, null)
         };
+
+    private bool IsScopedConstructorEnabled(CompositionCode composition)
+    {
+        var scopeNodes =
+            from node in composition.Source.Graph.Vertices
+            where node.ActualLifetime == Lifetime.Scoped
+            where registry.IsRegistered(composition.Source.Source, node.Binding.Id)
+            select node;
+
+       return scopeNodes.Any();
+    }
 }
