abe55dd3c0b5ce8f91deed49971bd0b1d3075347
Nikolay Pianikov <nikolayp@mail.ru>
2026-03-21 08:13:50 +0300

    Refactor `GetCyclicNode` implementation for improved performance and clarity

--- src/Pure.DI.Core/Core/Code/CycleTools.cs
+++ src/Pure.DI.Core/Core/Code/CycleTools.cs
@@ -4,34 +4,60 @@ class CycleTools : ICycleTools
 {
     public DependencyNode? GetCyclicNode(
         IGraph<DependencyNode, Dependency> graph,
-        DependencyNode node) =>
-        GetCyclicNode(graph, node, ImmutableHashSet<DependencyNode>.Empty);
-
-    private static DependencyNode? GetCyclicNode(
-        IGraph<DependencyNode, Dependency> graph,
-        DependencyNode node,
-        in ImmutableHashSet<DependencyNode> processed)
+        DependencyNode node)
     {
-        if (processed.Contains(node))
+        var visited = new HashSet<DependencyNode>();
+        var recursionStack = new HashSet<DependencyNode>();
+        var stack = new Stack<(DependencyNode node, IEnumerator<Dependency>? enumerator)>();
+        stack.Push((node, null));
+        while (stack.Count > 0)
         {
-            return node;
-        }
-
-        var newProcessed = processed.Add(node);
-        if (!graph.TryGetInEdges(node, out var dependencies))
-        {
-            return null;
-        }
-
-        // ReSharper disable once LoopCanBeConvertedToQuery
-        foreach (var dependency in dependencies)
-        {
-            if (GetCyclicNode(graph, dependency.Source, newProcessed) is {} cyclicNode)
+            var (currentNode, enumerator) = stack.Peek();
+            if (enumerator == null)
             {
-                return cyclicNode;
+                if (visited.Contains(currentNode) && !recursionStack.Contains(currentNode))
+                {
+                    stack.Pop();
+                    continue;
+                }
+                
+                visited.Add(currentNode);
+                recursionStack.Add(currentNode);
+                
+                if (!graph.TryGetInEdges(currentNode, out var dependencies))
+                {
+                    stack.Pop();
+                    recursionStack.Remove(currentNode);
+                    continue;
+                }
+                
+                stack.Pop();
+                // ReSharper disable once GenericEnumeratorNotDisposed
+                stack.Push((currentNode, dependencies.GetEnumerator()));
+            }
+            else
+            {
+                if (!enumerator.MoveNext())
+                {
+                    enumerator.Dispose();
+                    stack.Pop();
+                    recursionStack.Remove(currentNode);
+                    continue;
+                }
+                
+                var dependency = enumerator.Current;
+                if (!visited.Contains(dependency.Source))
+                {
+                    stack.Push((dependency.Source, null));
+                }
+                else if (recursionStack.Contains(dependency.Source))
+                {
+                    enumerator.Dispose();
+                    return dependency.Source;
+                }
             }
         }
-
+        
         return null;
     }
 }
