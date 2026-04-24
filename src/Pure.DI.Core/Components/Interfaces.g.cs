54591aad1570f55547d11c7c172bfa7eb87a733e
Nikolay Pianikov <nikolayp@mail.ru>
2026-04-24 18:37:35 +0300

    Refactor: Update `IgnoreInterface` example to clarify excluded state terminology

--- src/Pure.DI.Core/Components/Interfaces.g.cs
+++ src/Pure.DI.Core/Components/Interfaces.g.cs
@@ -64,7 +64,7 @@ namespace Pure.DI
     ///     public void Run() { }
     ///
     ///     [IgnoreInterface]
-    ///     public string InternalState => "hidden";
+    ///     public string InternalState => "excluded";
     /// }
     /// </code>
     /// </example>
